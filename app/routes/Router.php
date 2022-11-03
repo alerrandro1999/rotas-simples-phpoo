4717ad65d27a2ec80c99d07ac1593e91a65d0022
alerrandro borges <alerrandrokaton@gmail.com>
2022-11-03 17:49:25 -0300

    Feat: controler

--- app/routes/Router.php
+++ app/routes/Router.php
@@ -3,9 +3,27 @@ namespace app\routes;
 
 class Router
 {
-    public static function load(string $controller, string $action)
+    const CONTROLLER_NAMESPACE = 'app\\controllers';
+    public static function load(string $controller, string $method)
     {
+        try {
+            //verificar se o controller existe
+            $controllerNamespace = self::CONTROLLER_NAMESPACE.'\\'.$controller;
+            if (!class_exists($controllerNamespace)) {
+                throw new \Exception("O Controller {$controller} não existe");
+            }
 
+            $controllerInstance = new $controllerNamespace;
+
+            if (!method_exists($controllerInstance, $method)) {
+                throw new \Exception("O método {$method} não existe no Controller {$controller}");
+            }
+
+            $controllerInstance->$method();
+
+        } catch (\Throwable $th) {
+            echo $th->getMessage();
+        }
     }
     public static function  routes() : array
     {
@@ -21,7 +39,6 @@ class Router
 
             ],
             'delete' => [
-
             ]
             ];
     }
